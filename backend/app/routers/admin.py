4bb9893d3f18e93cd6186ce74c4830412bfb4af4
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-04 17:29:04 +0530

    Update Admin Router

--- backend/app/routers/admin.py
+++ backend/app/routers/admin.py
@@ -1,23 +1,13 @@
 from fastapi import APIRouter
 from app.schemas.admin import AdminLogin
+from app.auth.admin_login import AdminAuth
 
 router = APIRouter(
     prefix="/admin",
     tags=["Admin"]
 )
 
+
 @router.post("/login")
 def admin_login(admin: AdminLogin):
-    if (
-        admin.email == "admin@firstchoiceinfra.com"
-        and admin.password == "Admin@123"
-    ):
-        return {
-            "success": True,
-            "message": "Admin Login Successful"
-        }
-
-    return {
-        "success": False,
-        "message": "Invalid Email or Password"
-    }
+    return AdminAuth.login(admin)
