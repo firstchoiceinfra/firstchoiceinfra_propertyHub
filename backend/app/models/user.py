4d8da0cffecb5b9ecb44023c3beabf80d4e82ab3
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-04 17:42:47 +0530

    Add User Model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,1 +1,8 @@
+from pydantic import BaseModel
+from typing import Literal
 
+
+class User(BaseModel):
+    full_name: str
+    mobile_number: str
+    role: Literal["Buyer", "Owner", "Agent", "Builder"]
