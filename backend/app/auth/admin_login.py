7c338ee83b8451c388f67cad5c848d4cdfdcdcda
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-04 17:24:59 +0530

    Update admin_login.py

--- backend/app/auth/admin_login.py
+++ backend/app/auth/admin_login.py
@@ -1,1 +1,23 @@
+from app.schemas.admin import AdminLogin
 
+
+class AdminAuth:
+
+    @staticmethod
+    def login(admin: AdminLogin):
+
+        if (
+            admin.email == "admin@firstchoiceinfra.com"
+            and admin.password == "Admin@123"
+        ):
+
+            return {
+                "success": True,
+                "message": "Login Successful",
+                "role": "Admin"
+            }
+
+        return {
+            "success": False,
+            "message": "Invalid Email or Password"
+        }
