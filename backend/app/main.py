8325572795e01c7c104ee97cc950e4edea804e63
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-04 17:22:40 +0530

    Connect Admin Router to FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
+from app.routers.admin import router as admin_router
 
 app = FastAPI(
     title="FirstChoice Infra PropertyHub",
     version="1.0.0"
 )
 
+app.include_router(admin_router)
+
+
 @app.get("/")
 def home():
     return {
+        "success": True,
         "message": "Welcome to FirstChoice Infra PropertyHub"
     }
+
 
 @app.get("/health")
 def health():
     return {
-        "status": "Server Running"
+        "success": True,
+        "message": "Server Running Successfully"
     }
