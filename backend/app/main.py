d985f65ac1ae456fb97a157bbd33910bdc1e9a0f
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-04 17:13:01 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,19 @@
 
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="FirstChoice Infra PropertyHub",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to FirstChoice Infra PropertyHub"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "Server Running"
+    }
