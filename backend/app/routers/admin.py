444b40cc6d1a60607d18c87a6fcb65f0b1c75c31
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-04 17:19:24 +0530

    Add Admin login API

--- backend/app/routers/admin.py
+++ backend/app/routers/admin.py
@@ -1,1 +1,23 @@
+from fastapi import APIRouter
+from app.schemas.admin import AdminLogin
 
+router = APIRouter(
+    prefix="/admin",
+    tags=["Admin"]
+)
+
+@router.post("/login")
+def admin_login(admin: AdminLogin):
+    if (
+        admin.email == "admin@firstchoiceinfra.com"
+        and admin.password == "Admin@123"
+    ):
+        return {
+            "success": True,
+            "message": "Admin Login Successful"
+        }
+
+    return {
+        "success": False,
+        "message": "Invalid Email or Password"
+    }
