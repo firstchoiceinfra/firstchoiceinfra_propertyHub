f099da6e4544c51dfd48f64f65a3b5feb69ae803
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-04 17:49:11 +0530

    Add User Registration Router

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+from app.schemas.user import UserRegistration
+from app.auth.user_auth import UserAuth
 
+router = APIRouter(
+    prefix="/user",
+    tags=["User"]
+)
