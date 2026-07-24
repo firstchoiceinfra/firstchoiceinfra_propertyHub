9ed067b19cff12ee2b293390378a7f73cf3624d2
firstchoiceinfra <Jituparate9326@gmail.com>
2026-07-24 16:18:24 +0530

    Delete core/settings.py

--- core/settings.py
+++
@@ -1,9 +0,0 @@
-APP_NAME = "FirstChoice Infra Property Hub"
-
-APP_VERSION = "1.0.0"
-
-COMPANY_NAME = "FirstChoice Infra"
-
-COUNTRY = "India"
-
-DEFAULT_PAGE_TITLE = "FirstChoice Infra Property Hub"
